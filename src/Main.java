40971cf351450d5df4b5137396c7adfb27730631
nsimpson978 <150094797+nsimpson978@users.noreply.github.com>
2025-11-09 09:59:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,31 @@
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
     public static void main(String[] args) {
-       launch(args); 
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage){
-
+    public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("/Journal.fxml"));
             AnchorPane root = loader.load();
-
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
-
+            primaryStage.setTitle("AI Mood Journal");
             primaryStage.show();
         } catch (IOException e) {
-            System.err.println("COULD NOT LOAD FXML: " + e.getMessage());
+            e.printStackTrace(); 
         }
     }
-    
 }
+
+
+
+
